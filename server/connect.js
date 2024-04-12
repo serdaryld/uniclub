709abbe12fc9627de02b7d448911ed432c5c594c
Serdar Yıldırım <135362663+serdaryld@users.noreply.github.com>
2024-04-12 21:05:35 +0300

    Update connect.js

--- server/connect.js
+++ server/connect.js
@@ -3,6 +3,6 @@ import mysql from "mysql2"
 export const db = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"dtgegp314159@Q",
-    database:"vtysproje"
+    password:"password",
+    database:"name"
 })
